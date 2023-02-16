3f9b677e20bdef82036b2362ddcd37723bcb0da1
Khup Taithul <aatuang@gmail.com>
2023-02-16 16:38:06 +0000

    fixed the unchecked exception

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -14,6 +14,8 @@ public class Lab {
      * event happens, like running out of computer memory.
      */
     public void throwUncheckedException(){
-
+        int a = 1;
+        int b = 0;
+        int result = a/b;
     }
 }
